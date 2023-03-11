ea1ee2617592fedda2717e304b023e44d8cde3d9
Silva Vanessa (SO/OPM43-BR) <isa7ca@bosch.com>
2023-03-11 15:06:21 -0300

    semaphore

---
+++ correcaoSemafaro/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace correcaoSemafaro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::correcaoSemafaro.Properties.Resources.vermelho;
+            this.pictureBox1.Location = new System.Drawing.Point(92, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(131, 107);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::correcaoSemafaro.Properties.Resources.amarelo;
+            this.pictureBox2.Location = new System.Drawing.Point(92, 163);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(131, 112);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::correcaoSemafaro.Properties.Resources.verde;
+            this.pictureBox3.Location = new System.Drawing.Point(92, 281);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(131, 117);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::correcaoSemafaro.Properties.Resources.ped_verde;
+            this.pictureBox4.Location = new System.Drawing.Point(327, 116);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(188, 282);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 450);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ correcaoSemafaro/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace correcaoSemafaro
+{
+    public partial class Form1 : Form
+    {
+        Timer timer1 = new Timer();
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void inicia()
+        {
+            timer1.Interval = 1000;
+            timer1.Tick += new EventHandler(sema);
+            timer1.Enabled = true;
+
+        }
+        public void sema(object sender, EventArgs e)
+        {
+            if (count < 6)
+            {
+                pictureBox1.Image = Properties.Resources.vermelho_aceso;
+                pictureBox4.Image = Properties.Resources.ped_verde;
+                count++;
+            }
+            else if (count < 8)
+            {
+                pictureBox1.Image = Properties.Resources.vermelho;
+                pictureBox2.Image = Properties.Resources.amarelo_aceso;
+                pictureBox4.Image = Properties.Resources.ped_vermelho;
+                count++;
+            }
+            else if (count < 13)
+            {
+                pictureBox2.Image = Properties.Resources.amarelo;
+                pictureBox3.Image = Properties.Resources.verde_aceso;
+                count++;
+            }
+            else
+            {
+                pictureBox1.Image = Properties.Resources.vermelho_aceso;
+                pictureBox3.Image = Properties.Resources.verde;
+                count = 0;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            inicia();
+        }
+    }
+}
